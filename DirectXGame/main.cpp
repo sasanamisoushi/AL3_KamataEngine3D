c4c06674e7fddc2dec580993a57f929866e8bb21
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-04-18 12:08:28 +0900

    リセット

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,64 +1,42 @@
 #include <Windows.h>
-#include "KamataEngine.h"
+
 #include "GameScene.h"
+#include "KamataEngine.h"
 
 using namespace KamataEngine;
 
-//Windowsアプリでのエントリーポイント(main関数)
+// Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
-	
-	//エンジンの初期化
+
+	// エンジンの初期化
 	KamataEngine::Initialize();
-	//ゲームシーンのインスタンスの生成
+	// ゲームシーンの初期化
 	GameScene* gameScene = new GameScene();
-	//ゲームシーンの初期化
-	gameScene->Initialize();
-	// DirectXCommonインスタンスの取得
-	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-	//ImGuiManagerインスタンスの取得
-	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
-		
-	//メインループ
+	// メインループ
 	while (true) {
-		//エンジンの更新
+		// エンジンの更新
 		if (KamataEngine::Update()) {
 			break;
 		}
-		
-		//ImGui受付開始
-		imguiManager->Begin();
 
-		//ゲームシーンの更新
-		gameScene->Update();
+		// DirectXCommonインスタンスの取得
+		DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-		//ImGui受付終了
-		imguiManager->End();
-
-		//描画の開始
+		// 描画の開始
 		dxCommon->PreDraw();
 
 		gameScene->Draw();
 
-		//軸表示の描画
-		AxisIndicator::GetInstance()->Draw();
-
-
-		//ImGui描画
-		imguiManager->Draw();
-
-		//描画終了
+		// 描画終了
 		dxCommon->PostDraw();
 	}
 
-	//ゲームシーンの解放
+	// ゲームシーンの解放
 	delete gameScene;
-	//nullptrの代入
+	// nullptrの代入
 	gameScene = nullptr;
 
-	
-
-	//エンジンの終了処理
+	// エンジンの終了処理
 	KamataEngine::Finalize();
 	return 0;
 }
