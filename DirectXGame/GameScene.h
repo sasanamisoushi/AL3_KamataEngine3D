6f7552ad7bdb6ffde84a1c75e6d8f8880c4ce6ef
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-04-18 12:45:49 +0900

    playerのクラスを追加した

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -12,4 +12,21 @@ public:
 
 	// 描画
 	void Draw();
+
+	~GameScene();
+
+private:
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//3Dモデルデータ
+	KamataEngine::Model* model_ = nullptr;
+
+	//ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+
+	//カメラ
+	KamataEngine::Camera camera_;
+
+
 };
