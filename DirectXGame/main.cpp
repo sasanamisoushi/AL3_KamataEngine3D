b1414e303e16a5169e0ab23c36167a8cc3714fa3
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-04-18 12:00:51 +0900

    01_03完成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -10,25 +10,42 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
 	//エンジンの初期化
 	KamataEngine::Initialize();
-	//ゲームシーンの初期化
+	//ゲームシーンのインスタンスの生成
 	GameScene* gameScene = new GameScene();
+	//ゲームシーンの初期化
+	gameScene->Initialize();
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	//ImGuiManagerインスタンスの取得
+	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
+		
 	//メインループ
 	while (true) {
 		//エンジンの更新
 		if (KamataEngine::Update()) {
 			break;
 		}
-
-		//DirectXCommonインスタンスの取得
-		DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+		
+		//ImGui受付開始
+		imguiManager->Begin();
 
 		//ゲームシーンの更新
 		gameScene->Update();
+
+		//ImGui受付終了
+		imguiManager->End();
 
 		//描画の開始
 		dxCommon->PreDraw();
 
 		gameScene->Draw();
+
+		//軸表示の描画
+		AxisIndicator::GetInstance()->Draw();
+
+
+		//ImGui描画
+		imguiManager->Draw();
 
 		//描画終了
 		dxCommon->PostDraw();
