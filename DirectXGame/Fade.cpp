3c77870e3093d42add7891d201064052ce4956ae
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-07-04 13:05:41 +0900

    スライド21ページまで完成

--- DirectXGame/Fade.cpp
+++ DirectXGame/Fade.cpp
@@ -1,4 +1,5 @@
 #include "Fade.h"
+#include <algorithm>
 
 using namespace KamataEngine;
 
@@ -11,7 +12,36 @@ void Fade::Initialize() {
 
 }
 
-void Fade::Update() {}
+void Fade::Update() {
+	//フェード状態による分岐
+	switch (status_) {
+	case Status::None:
+		break;
+	case Status::FadeIn:
+		// 1フレーム分の秒数をカウントアップ
+		counter_ += 1.0f / 60.0f;
+		// フェード継続時間に達したら打ち止め
+		if (counter_ >= duration_) {
+			counter_ = duration_;
+		}
+
+		// 0.0fから1.0fの間で、経過時間がフェード継続時間に近づくほどアルファ値を大きくする
+		sprite_->SetColor(Vector4(0, 0, 0, std::clamp(1.0f - counter_ / duration_, 0.0f, 1.0f)));
+		break;
+	case Status::FedeOut:
+		//1フレーム分の秒数をカウントアップ
+		counter_ += 1.0f / 60.0f;
+		//フェード継続時間に達したら打ち止め
+		if (counter_ >= duration_) {
+			counter_ = duration_;
+		}
+
+		//0.0fから1.0fの間で、経過時間がフェード継続時間に近づくほどアルファ値を大きくする
+		sprite_->SetColor(Vector4(0, 0, 0, std::clamp(counter_ / duration_, 0.0f, 1.0f)));
+		break;
+	}
+
+}
 
 void Fade::Draw() {
 
@@ -20,4 +50,10 @@ void Fade::Draw() {
 	Sprite::PostDraw();
 
 
+}
+
+void Fade::Start(Status status, float duration) {
+	status_ = status;
+	duration_ = duration;
+	counter_ = 0.0f;
 }
--- DirectXGame/Fade.h
+++ DirectXGame/Fade.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "KamataEngine.h"
+
 class Fade {
 public:
 	void Initialize();
@@ -8,6 +9,24 @@ public:
 
 	void Draw();
 
+	enum class Status {
+		None,      //フェード無し
+		FadeIn,    //フェードイン中
+		FedeOut,   //フェードアウト中
+	};
+
 private:
 	KamataEngine::Sprite* sprite_ = nullptr;
+
+	//現在のフェード状態
+	Status status_ = Status::None;
+
+	//フェードの持続時間
+	float duration_ = 0.0f;
+
+	//経過時間カウンター
+	float counter_ = 0.0f;
+
+	//フェード開始
+	void Start(Status status, float duration);
 };
